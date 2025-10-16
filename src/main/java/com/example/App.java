2b018bc818339a32c84f1ed156c2b44f51611f50
yogi7302 <mpyogi2226@gmail.com>
2025-10-16 19:48:49 +0530

    Update console message for Jenkins webhook build

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,8 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("🚀 Hello from Jenkins + Maven + Webhook!");
+        System.out.println("🚀 Jenkins Webhook triggered build successfully!");
+
     }
 }
 
