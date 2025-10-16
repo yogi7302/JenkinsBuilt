85768608763aa6e849579b8a0e3b814d90d57ce7
yogi7302 <mpyogi2226@gmail.com>
2025-10-16 19:38:33 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Build!");
+        System.out.println("🚀 Hello from Jenkins + Maven + Webhook!");
     }
 }
+
